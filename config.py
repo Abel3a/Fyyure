467794aa8242ce501993ad0c22cb1c8e81d7b2b0
Abel AGBONON <72312401+Abel3a@users.noreply.github.com>
2022-10-28 21:01:47 +0100

    Add files via upload

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
+
 import os
+import re
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -7,7 +9,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-
+'''uri = os.getenv("postgres://postgres:abel@localhost:5432/fyyure") 
+ # or other relevant config var
+if uri.startswith("postgres://postgres:abel@localhost:5432/fyyure"):
+  uri = uri.replace("postgres://postgres:abel@localhost:5432/fyyure", "postgresql://postgres:abel@localhost:5432/fyyure", 1)'''
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = "postgresql://postgres:abel@localhost:5432/fyyure"
+SQLALCHEMY_TRACK_MODIFICATIONS=True
